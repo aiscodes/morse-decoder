33d201869303f0ddefb7588f7485b2d59905a5a3
aiscodes <aigerim6769@gmail.com>
2022-02-02 11:23:12 +0600

    task is done

--- src/index.js
+++ src/index.js
@@ -38,7 +38,33 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+
+    let morseCode = [];
+    for (let i = 0; i <= expr.length+1; i += 10) {
+      const first = expr.slice(i, i + 10);
+      const second = first.slice(first.indexOf("1"));
+      let result = "";
+      for (let i = 0; i <= second.length+1; i += 2) {
+          const each = second.slice(i, i + 2);
+          switch (each) {
+              case "10":
+                  result += ".";
+                  break;
+              case "11":
+                  result += "-";
+                  break;
+              case "*":
+                  result += " ";
+                  break;
+          }
+      }
+      morseCode.push(result);
+    }
+
+    return morseCode
+      .map(el => {return el === " " ? " " : MORSE_TABLE[el]})
+      .join("");
+  
 }
 
 module.exports = {
